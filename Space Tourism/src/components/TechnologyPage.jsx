c83a837c04c6dba2fbd170829154165f6ada07d4
markjay101 <markjayfuentescuyos1@gmail.com>
2023-08-05 11:47:46 +0800

    update sources path

--- Space Tourism/src/components/TechnologyPage.jsx
+++ Space Tourism/src/components/TechnologyPage.jsx
@@ -2,10 +2,10 @@ import technologyInfo from "../assets/data/data.json";
 import { useState } from "react";
 export default function TechnologyPage() {
   const [technologyImgPortrait, setTechnologyImgPortrait] = useState(
-    "./src/assets/technology/image-launch-vehicle-portrait.jpg"
+    "../assets/technology/image-launch-vehicle-portrait.jpg"
   );
   const [technologyImgLandscape, setTechnologyImgLandscape] = useState(
-    "./src/assets/technology/image-launch-vehicle-landscape.jpg"
+    "../assets/technology/image-launch-vehicle-landscape.jpg"
   );
 
   function handleClick(portrait, landscape) {
